2639c677649f5465ad4db16b74f39abca6bc02aa
Jordane Lew <jordane.lew@gmail.com>
2021-04-15 22:09:49 +1200

    Fix sourcing bashrc before configuring bash-it. Fix micro path export

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
-
-sed -i 's/'"$BASH_IT_THEME"'/sexy/g' ~/.bashrc
+. ~/.bashrc
 
 bash-it enable alias git
-
 bash-it enable plugin git
 bash-it enable plugin gitstatus
 bash-it enable plugin thefuck
+sed -i 's/'"$BASH_IT_THEME"'/sexy/g' ~/.bashrc
 
 mkdir ~/micro && cd ~/micro && curl https://getmic.ro | bash
-echo "PATH=$PATH:~/micro" >> ~/.bashrc
+echo 'export PATH="$HOME/micro:$PATH"' >> ~/.bashrc
