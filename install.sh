9b19d35b354e7b2437d4279520c3b4a2691e1806
Jordane Lew <jordane.lew@gmail.com>
2021-04-15 22:28:18 +1200

    Flesh out README. Drop thefuck, restart bash-it after config

--- install.sh
+++ install.sh
@@ -5,8 +5,8 @@ git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 bash-it enable alias git
 bash-it enable plugin git
 bash-it enable plugin gitstatus
-bash-it enable plugin thefuck
 sed -i 's/'"$BASH_IT_THEME"'/sexy/g' ~/.bashrc
+bash-it restart
 
 mkdir ~/micro && cd ~/micro && curl https://getmic.ro | bash
 echo 'export PATH="$HOME/micro:$PATH"' >> ~/.bashrc
