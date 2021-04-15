45f0ed6c31558f9f7c2ee24f1cdf544333bad2ce
Jordane Lew <jordane.lew@gmail.com>
2021-04-15 10:38:08 +0000

    Source bash-it instead of bashrc for bash-it init

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
 ~/.bash_it/install.sh --silent
-. ~/.bashrc
+
+~/.bash_it/bash_it.sh
 
 bash-it enable alias git
 bash-it enable plugin git
 bash-it enable plugin gitstatus
 sed -i 's/'"$BASH_IT_THEME"'/sexy/g' ~/.bashrc
-bash-it restart
 
 mkdir ~/micro && cd ~/micro && curl https://getmic.ro | bash
 echo 'export PATH="$HOME/micro:$PATH"' >> ~/.bashrc
